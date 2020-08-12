d2ff7160fd53c2eaf164ec355ce4579f3801ac8a
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-08-12 11:21:48 +0700

    code stick Navigation va Scroll

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,9 +1,27 @@
 $(document).ready(
     function(){
-        $('h1').click(
-            function(){
-                $(this).css('color','red');
+        /* cách about section 600px thì hiện sticky*/
+        // sticky nav
+        $('.about-section').waypoint(
+            function(direction){
+                if(direction == "down"){
+                    $('nav').addClass('sticky');
+                } else
+                {
+                    ('nav').removeClass('sticky');
+                }
+            },{
+                offset: '600px'
             }
         )
+
+        // Scroll - hieu ung chuyen khi click
+        $('a').click(function(){
+            $('html, body').animate({
+              scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 700);
+            event.preventDefault();
+          });
     }
 )
+
